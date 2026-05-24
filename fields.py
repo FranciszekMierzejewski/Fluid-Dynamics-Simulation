501aa7d10e0daf430960a9abc7bb0863f2dee8fc
FranciszekMierzejewski <franek.mski@gmail.com>
2026-05-24 20:17:03 +0100

    Added fields.py file

---
+++ fields.py
@@ -0,0 +1,9 @@
+import numpy as np
+from grid import Grid
+
+class Fields():
+    def __init__(self, grid: Grid):
+        """
+        Stores three physical fields of the fluid, the x velocity component, y velocity component, and pressure. 
+        """
+        pass
