49a63b2d3409e151a47e133d34dbc90024f331a6
FranciszekMierzejewski <franek.mski@gmail.com>
2026-05-24 22:59:30 +0100

    Added max velocity, resultant velocity, resultant angle functions and string representation of Fields object

--- fields.py
+++ fields.py
@@ -16,3 +16,30 @@ class Fields():
         self.x = self.grid.grid_zeros()
         self.y = self.grid.grid_zeros()
         self.p = self.grid.grid_zeros()
+
+    def max_velocity(self) -> float:
+        return max(np.max(np.abs(self.x)), np.max(np.abs(self.y))) # take modulus of each cell in each velocity field, take max of each field, and compare max
+    
+    def resultant_velocity(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Calculate resulatant velocity from x and y components of each cell in the velocity fields
+        """
+        return np.sqrt(x**2 + y**2) 
+
+    def resultant_angle(self) -> np.ndarray:
+        """
+        Calculate angle of resultant velocity of each cell in the velocity fields, in radians.
+        
+        Domain of arctan = (-∞, ∞)
+        Range of arctan = (-π/2, π/2)
+        """
+        return np.arctan(self.resultant_velocity(self.x, self.y)) 
+
+    def __repr__(self) -> str:
+        #return f"Fields: \nx=\n{self.x}, \ny=\n{self.y}, \np=\n{self.p}"
+        return f"Resultant Field: \n{self.resultant_velocity(self.x, self.y)}\n Resultant Angle: \n {self.resultant_angle()}"
+    
+if __name__ == "__main__":
+    grid = Grid(100,100,1.0,1.0)
+    fields = Fields(grid)
+    print(fields)
