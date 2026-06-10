49edd85f5b0d156a76c992ab37662c71f685c22c
FranciszekMierzejewski <franek.mski@gmail.com>
2026-06-10 19:59:29 +0100

    Added SOR with residual for convergence check. Need to work on source term func to pass into SOR

--- pressure_projection.py
+++ pressure_projection.py
@@ -5,53 +5,52 @@ from grid import Grid
 from fields import Fields
 
 class PressureProjection():
-    def __init__(self, fields: Fields):
+    def __init__(self, fields: Fields, omega, tolerance: float = 1e-6, max_iterations: int = 10000):
         self.fields = fields
         self.grid = fields.grid
+        self.tolerance = tolerance
+        self.max_iterations = max_iterations
 
-    def calculate_divergence(self) -> np.ndarray:
+        max_mean: float = 1/2 * (np.cos(np.pi/self.grid.number_of_rows) + np.cos(np.pi/self.grid.number_of_columns))
+        self.omega: float = 2/(1 + (1 - max_mean)** 0.5)
+
+
+    def _calculate_divergence(self) -> np.ndarray:
         return self.grid.divergence(self.fields.x, self.fields.y)
-
-    def calculate_pressure(self):
-        """
-        ∇²p = (ρ/dt) ∇·u
-        x_i^(k+1) = x_i^(k) + ω((x_i^(k+1))_GS - x_i^(k))
-        I will use Successive Over-Relexation (1 < ω < 2), a variant of the Gauss-Seidel method, which computes a weighted average of previous and new iterate, allowing for faster convergence.
-        
-        For N^2 grid: 
-          ω_opt ~~ 2/(1 + (1 - cos(π/N)^1/2) = 2/(1 + sin(π/N))
-        """
-
-        # for each iteration, go across each cell in roster order and update pressure 
-        max_mean = 1/2 * (np.cos(np.pi/self.grid.number_of_rows) + np.cos(np.pi/self.grid.number_of_columns))
-        optimal_weight = 2/(1 + (1 - max_mean)** 0.5)
     
+    def _calculate_source_term(self, dt: float) -> np.ndarray:
+        return self._calculate_divergence() / dt
+    
+    def _calculate_successive_over_relaxation(self, b: np.ndarray):
+        if self.fields.p is None:
+            raise ValueError("Pressure field is not initialized")
+        
+        p = self.fields.p
+        dx = (self.grid.cell_x_length, 1.0)[self.grid.cell_x_length is None]
+        dy = (self.grid.cell_y_length, 1.0)[self.grid.cell_y_length is None]
+        dx_squared = dx ** 2
+        dy_squared = dy ** 2
+        denominator = 2 * (dx_squared + dy_squared)
 
-"""
-import numpy as np
-def SOR(A, b, x, w):
-  A = np.array(A); b = np.array(b); x = np.array(x);
-  n = len(A)
-  xnew = np.zeros(n)
-  for i in range(n):
-    xnew[i] = (1 - w)*x[i] + w*(b[i] - sum([A[i, j]*x[j] for j in range(i + 1, n)]) - sum([A[i, j]*xnew[j] for j in range(i)]))/A[i, i]
-  return xnew
-  
-A = [[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]]
-b = [7.85, -19.3, 71.4]
-x = [[1, 1, 1.]]
-MaxIter = 100
-ErrorTable = [1]
-eps = 0.001
-i = 1
-omega = 1.1
+        for iteration in range(self.max_iterations):
+            p_old = p.copy()
 
-while i <= MaxIter and abs(ErrorTable[i - 1]) > eps: 
-  xi = SOR(A, b, x[i - 1],omega)
-  x.append(xi)
-  ei = np.linalg.norm(x[i] - x[i - 1])/np.linalg.norm(x[i]) 
-  ErrorTable.append(ei) 
-  i+=1
-print("x:",np.array(x))
-print("ErrorTable:",np.vstack(ErrorTable))
-"""
+            for i in range(1, self.grid.grid_shape[0] - 1):
+                for j in range(1, self.grid.grid_shape[1] - 1):
+                    p[i, j] = (
+                      self.omega * 
+                      ((p[i+1, j] + p[i-1, j]) * dy_squared + 
+                      (p[i, j+1] + p[i, j-1]) * dx_squared - 
+                      b[i, j] * dx_squared * dy_squared) / denominator + 
+                      (1 - self.omega) * p[i, j]
+                    )
+
+            residual = np.linalg.norm(p - p_old)/np.linalg.norm(p)
+            
+            if residual < self.tolerance:
+                print(f"Successive over relaxation with weight {self.omega:.4f} has converged in {iteration + 1} iterations with residual {residual:.2e}")
+                break
+        print(f"Reached maximum iterations {self.max_iterations}, failed to converge")
+            
+        return p
+
